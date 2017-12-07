48b0374b41344dfb0123a699f0d60c1ec38cf687
Matthew Haddock <777mth@gmail.com>
2017-12-07 07:04:55 -0500

    reorganize with Event Listeners

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,136 +1,130 @@
-var dictionary = ["survivor","directory","wisecrack","survival","application","parachute","innocent","consideration","discover","cylinder","liability","majority","circumstance","flatware","interrupt","transfer","skeleton","craftsman","multimedia","hardship","implicit","compound","inhabitant","finished","depressed","reduction","triangle","fountain","correspondence","unanimous","umbrella","elaborate","revolutionary","confrontation","horseshoe","opponent","manufacturer","undermine","director","sentence","patience","expression","competition","flourish","feminist","important","anticipation","disappoint","unpleasant","dressing","emphasis","difference","astonishing","insurance","ostracize","policeman","microphone","shareholder","handicap","eliminate","orthodox","experiment","photograph","volunteer","transition","horoscope","compartment","ambiguity","presidency","scenario","reinforce","advertise","classroom","prevalence","expenditure","conception","abstract","theorist","domestic","disgrace","ministry","attitude","computer","corruption","communication","weakness","beginning","conflict","electronics","convulsion","personality","simplicity","midnight","accompany","reaction","necklace","publisher","potential","intelligence","worthless","door","needless","pathetic","stain","loaf","splendid","grumpy","puny","rainy","interfere","impartial","uppity","humorous","purpose","workable","thrill","nail","periodic","vein","warn","lively","religion","jittery","merciful","snow","relax","gratis","defiant","innocent","skinny","laugh","elated","silk","turkey","collar","sneaky","jazzy","punishment","canvas","cute","examine","unsightly","duck","fail","cushion","produce","hunt","cute","ring","cat","jellyfish","rabbits","entertain","itchy","outstanding","race","bead","art","committee","bless","ready","birthday","grin","load","noiseless","replace","gamy","serious","tow","day","effect","faint","resonant","book","wall","mere","consist","hop","solid","clean","seat","elastic","apparatus","quilt","scold","dinosaurs","rinse","rot","panicky","sable","credit","sigh","honorable","correct","unwieldy"];
 
-    var usedLetters = [];
-    var currentWord = [];
-    var maskedWord;
-    var alertDisplay = "<p>Waiting on you, chief.</p>";
-    var usedDisplay = "";
-    var strikes = 0;
-    var userInput = "";
-    var hardcore;
+// establish global variables
+var dictionary = ["survivor", "directory", "wisecrack", "survival", "application", "parachute", "innocent", "consideration", "discover", "cylinder", "liability", "majority", "circumstance", "flatware", "interrupt", "transfer", "skeleton", "craftsman", "multimedia", "hardship", "implicit", "compound", "inhabitant", "finished", "depressed", "reduction", "triangle", "fountain", "correspondence", "unanimous", "umbrella", "elaborate", "revolutionary", "confrontation", "horseshoe", "opponent", "manufacturer", "undermine", "director", "sentence", "patience", "expression", "competition", "flourish", "feminist", "important", "anticipation", "disappoint", "unpleasant", "dressing", "emphasis", "difference", "astonishing", "insurance", "ostracize", "policeman", "microphone", "shareholder", "handicap", "eliminate", "orthodox", "experiment", "photograph", "volunteer", "transition", "horoscope", "compartment", "ambiguity", "presidency", "scenario", "reinforce", "advertise", "classroom", "prevalence", "expenditure", "conception", "abstract", "theorist", "domestic", "disgrace", "ministry", "attitude", "computer", "corruption", "communication", "weakness", "beginning", "conflict", "electronics", "convulsion", "personality", "simplicity", "midnight", "accompany", "reaction", "necklace", "publisher", "potential", "intelligence", "worthless", "door", "needless", "pathetic", "stain", "loaf", "splendid", "grumpy", "puny", "rainy", "interfere", "impartial", "uppity", "humorous", "purpose", "workable", "thrill", "nail", "periodic", "vein", "warn", "lively", "religion", "jittery", "merciful", "snow", "relax", "gratis", "defiant", "innocent", "skinny", "laugh", "elated", "silk", "turkey", "collar", "sneaky", "jazzy", "punishment", "canvas", "cute", "examine", "unsightly", "duck", "fail", "cushion", "produce", "hunt", "cute", "ring", "cat", "jellyfish", "rabbits", "entertain", "itchy", "outstanding", "race", "bead", "art", "committee", "bless", "ready", "birthday", "grin", "load", "noiseless", "replace", "gamy", "serious", "tow", "day", "effect", "faint", "resonant", "book", "wall", "mere", "consist", "hop", "solid", "clean", "seat", "elastic", "apparatus", "quilt", "scold", "dinosaurs", "rinse", "rot", "panicky", "sable", "credit", "sigh", "honorable", "correct", "unwieldy"];
+var vanillaGameWins = 0;
+var vanillaGameTotal = 0;
+var hardcoreGameWins = 0;
+var hardcoreGameTotal = 0;
+var userInput = "";
+var hardcore = "false";
+var title = "Hangman";
+var alertDisplay;
+var currentWord = [];  
+var usedLetters = [];
+var strikes = 0;  
+var maskedWord = [];
+var lastGameWon = false;
 
-    var title = "Hangman";
-    // if (confirm("Are you truly HARD CORE?")) {
-    //   hardcore = true;
-    //   title = "<h1>Hardcore Hangman<h1>";
-    // }
+function initializeGame() {
+  currentWord = [];  
+  usedLetters = [];
+  strikes = 0;
+  document.querySelector("#endGameAlert").textContent = "";  
+  alertDisplay = "Waiting on you, chief.";
+  usedDisplay = "You ain't picked nothin' yet.";
+  var selectWord = dictionary[Math.floor(Math.random() * dictionary.length)];
+  for (i = 0; i < selectWord.length; i++) {
+    var letter = selectWord.charAt(i).toUpperCase();
+    currentWord.push(letter);
+  };
+};
 
-    // select word from dictionary
-    var selectWord = dictionary[Math.floor(Math.random() * dictionary.length)];
-
-    console.log(selectWord);
-    
-    // create currentWord array from selectWord
-    for (i = 0 ; i < selectWord.length ; i++) {
-      var letter = selectWord.charAt(i).toUpperCase();
-      currentWord.push(letter);
-    };
-    console.log("initial currentWord: " + currentWord);
-
-    // document.getElementById("restartButton").addEventListener("onmouseup", myFunction);
-
-    function displayRenders() {
-      maskedWord = "";
-      for (i = 0; i < currentWord.length; i++) {
-        if (usedLetters.includes(currentWord[i])) {
-          maskedWord = maskedWord + currentWord[i].toUpperCase() + " ";
-        } else {
-          maskedWord = maskedWord + "_ ";
-        }
-      };
-      console.log("initial maskedWord: " + maskedWord);
-      usedDisplay = "";
-      if (hardcore) {
-        usedDisplay = "Your last pick was '" + userInput.toUpperCase() + "'."
-      } else for (i = 0 ; i < usedLetters.length; i++) {
-        usedDisplay = usedDisplay + usedLetters[i].toUpperCase() + " ";
-      };
-      document.querySelector("#title").innerHTML = title;
-      document.querySelector("#maskedWord").innerHTML = maskedWord;
-      document.querySelector("#usedLetters").innerHTML = usedLetters;
-      document.querySelector("#alertDisplay").innerHTML = alertDisplay;
-      document.querySelector("#strikes").innerHTML = "<p>" + strikes + " / 7</p>";
-      document.querySelector("#gamesWon").innerHTML = gamesWon;
-      document.querySelector("#hardcoreGamesWon").innerHTML = hardcoreGamesWon;
-      // document.querySelector("backgroundImage").innerHTML = "<img src=\"http://www.placehold.it/350x150\" width=\"350\" height=\"150\">";
-
-    };
-
-    displayRenders();
-
-    html =
-        "<h1>" + title + "</h1>" +
-        "<p>" + maskedWord + "</p>" +
-        alertDisplay +
-        "<p>Used Letters: You ain't picked nothin'.</p>" + 
-        "<p>Strikes: " + strikes + " / 7</p>";
-    document.querySelector("#game").innerHTML = html;  
-
-    document.addEventListener = ("onkeyup", function(event) {
-      
-      userInput = event.key.toUpperCase();
-
-      if (!usedLetters.includes(userInput)) {
-        usedLetters.push(userInput);
-        usedLetters = usedLetters.sort();
-        if (!currentWord.includes(userInput)) {
-          strikes++;
-          alertDisplay = "<p>Nope, you're one step closer to stretching!</p>";
-        } else {
-          alertDisplay = "<p>Good choice!</p>";
-        };
-      } else {
-        alertDisplay = "<p>You've already used that letter!</p>";
-        if (hardcore) {strikes++;};
-      };
-      displayRenders();
-
-      html =
-        "<h1>" + title + "</h1>" +
-        "<p>" + maskedWord + "</p>" +
-        alertDisplay +
-        "<p>Used Letters: " + usedDisplay + "</p>" + 
-        "<p>Strikes: " + strikes + " / 7</p>";
-
-      document.querySelector("#game").innerHTML = html;  
-
-      // loss screen
-      if (strikes === 7) {
-        var unMaskedWord = "";
-        for (i = 0; i < currentWord.length; i++) {
-            unMaskedWord = unMaskedWord + currentWord[i].toUpperCase() + " ";
-        };
-
-        // hide solution from hardcore player
-        if (hardcore) {unMaskedWord = alertDisplay;};
-
-        html =
-        "<h1>" + title + "</h1>" +
-        "<p>" + maskedWord + "</p>" + // string declarations like this can't include expressions, can they?
-        "<p>" + unMaskedWord + "</p>" +
-        "<p>Used Letters: " + usedDisplay + "</p>" + 
-        "<p>Strikes: " + strikes + " / 7</p>" +
-        "<h2>You have met your deserved justice.</h2>";
-      }
-      
-      // win screen
-      else if (!maskedWord.includes("_")) {
-        html =
-        "<h1>" + title + "</h1>" +
-        "<p>" + maskedWord + "</p>" +
-        alertDisplay +
-        "<p>Used Letters: " + usedDisplay + "</p>" + 
-        "<p>Strikes: " + strikes + " / 7</p>" +
-        "<h2>You've escaped justice.</h2>";
-      };
-
-      document.querySelector("#game").innerHTML = html;
-    });
-
-    // document.removeEventListener("onkeyup" function(event))
-
-
-
-    // document.addEventListener = ("onkeyup", function(event) {});
-    
+function updateDisplay() {
+  maskedWord = "";
+  for (i = 0; i < currentWord.length; i++) {
+    if (usedLetters.includes(currentWord[i])) {
+      maskedWord = maskedWord + currentWord[i].toUpperCase() + " ";
+    } else {
+      maskedWord = maskedWord + "_ ";
     }
+  };
+  usedDisplay = "";
+  if (hardcore === true) {
+    usedDisplay = "Your last pick was '" + userInput.toUpperCase() + "'."
+  } else for (i = 0; i < usedLetters.length; i++) {
+    usedDisplay = usedDisplay + usedLetters[i].toUpperCase() + " ";
+  };
+  document.querySelector("#maskedWord").textContent = maskedWord;
+  document.querySelector("#usedDisplay").textContent = "Used Letters: " + usedDisplay;
+  document.querySelector("#alertDisplay").textContent = alertDisplay;
+  document.querySelector("#strikes").textContent = "Strikes: " + strikes + " / 7";
+  document.querySelector("#vanillaRecord").textContent = "Vanilla games won: " + vanillaGameWins + " / " + vanillaGameTotal;
+  document.querySelector("#hardcoreRecord").textContent = "Hardcore games won: " + hardcoreGameWins + " / " + hardcoreGameTotal;
+};
+// console.log(currentWord);
+
+
+
+
+function acceptInputs(event) {
+  userInput = event.key.toUpperCase();
+  console.log("acceptInputs starts");
+  console.log("userInput = " + userInput);
+  console.log("usedLetters = " + usedLetters);
+  console.log("currentWord = " + currentWord);
+  console.log("alertDisplay = " + alertDisplay);
+  console.log("strikes = " + strikes);
+  console.log("-");
+  if (!usedLetters.includes(userInput)) {
+    usedLetters.push(userInput);
+    usedLetters = usedLetters.sort();
+    if (!currentWord.includes(userInput)) {
+      strikes++;
+      alertDisplay = "Nope, you're one step closer to stretching!";
+    } else {
+      alertDisplay = "Good choice!";
+    };
+  } else {
+    alertDisplay = "You've already used that letter!";
+    if (hardcore) { strikes++; };
+  };
+  updateDisplay();
+  console.log("acceptInputs ends");
+  console.log("userInput = " + userInput);
+  console.log("maskedWord = " + maskedWord);
+  console.log("usedLetters = " + usedLetters);
+  console.log("currentWord = " + currentWord);
+  console.log("alertDisplay = " + alertDisplay);
+  console.log("strikes = " + strikes);
+  console.log("---");
+  if (strikes === 7) {
+    console.log(strikes);
+    console.log("dead!");
+    vanillaGameTotal++;
+    document.removeEventListener("keyup", acceptInputs);  
+    document.querySelector("#endGameAlert").textContent = "You lost! Press any key to start a new game!";
+    document.addEventListener("keyup", game);
+  }
+  else if (!maskedWord.includes("_")) {
+    vanillaGameTotal++;
+    vanillaGameWins++;
+    document.removeEventListener("keyup", acceptInputs);  
+    document.querySelector("#endGameAlert").textContent = "You won! Press any key to start a new game!";
+    document.addEventListener("keyup", game);
+    lastGameWon = true;
+
+  }
+};
+
+
+// at keypress, start game
+document.addEventListener("keyup", game);
+
+// display opening items
+document.querySelector("#title").textContent = title;
+document.querySelector("#alertDisplay").textContent = "Press any key to start!";
+
+function game(event) {
+  document.removeEventListener("keyup", game);
+  initializeGame();
+    console.log("end initializeGame");
+    console.log("alertDisplay = " + alertDisplay);
+    console.log("usedDisplay = " + usedDisplay);
+    console.log("currentWord = " + currentWord);
+    console.log("---");
+  updateDisplay();
+  document.addEventListener("keyup", acceptInputs)
+
+};
+
+
--- pseudocode.js
+++ pseudocode.js
@@ -1,27 +1,92 @@
-Stopping input watch?
-innerHTML or setAttribute with images?
-behavior of innerHTML with <p> tags? (view in console)
-
+Stopping input routine?
+how to change images if innerHTML isn't available?
 making game as object?
+
+to do:
+  use jQuery $("#maskedWord").append(currentWord[i]) to construct maskedWord
+
+
+function restartGame() {return;};
+jQuery $("#restartButton").on("click", restartGame);
+
+
+// USE FOR LOOP FOR GAME?
+  // Is this a bad way to use a "for" loop? (Turning it into a while...do loop)
+  // i.e. using condition to end loop without iterating (eliminating final expression)
+for (strikes = 7 ; strikes > 0 ; ) { 
+  var randomInput = Math.floor(Math.random() * 3);
+  console.log(randomInput);
+  if (randomInput != 2) {
+    console.log("good guess!");
+  } else {strikes--; console.log("bad guess!")};
+  console.log("strikes: " + strikes);
+};
+console.log("Done");
+
+var strikes = 3;
+
+while(strikes > 0){
+  var randomInput = Math.floor(Math.random() * 3);
+  console.log(randomInput);
+  if (randomInput != 2) {
+    console.log("good guess!");
+  } else {strikes--; console.log("bad guess!")};
+  console.log("strikes: " + strikes);
+  console.log("--------");  
+}
+console.log("Done");
 
 
 //  Opening Screen
-    //  establish global variables
-        dictionary = [];
-      //games = [];
-      //gameCounter = -1;
-      gamesWon = 0;
-      hardcoreGamesWon = 0;
-    "Press any key to start!"
-    //  render display
+//  establish global variables
+dictionary = [];
+//games = [];
+//gameCounter = -1;
+gamesWon = 0;
+hardcoreGamesWon = 0;
+function displayRenders() {
+  maskedWord = "";
+  for (i = 0; i < currentWord.length; i++) {
+    if (usedLetters.includes(currentWord[i])) {
+      maskedWord = maskedWord + currentWord[i].toUpperCase() + " ";
+    } else {
+      maskedWord = maskedWord + "_ ";
+    }
+  };
+  console.log("maskedWord: " + maskedWord);
+  usedDisplay = "";
+  if (hardcore) {
+    usedDisplay = "Your last pick was '" + userInput.toUpperCase() + "'."
+  } else for (i = 0 ; i < usedLetters.length; i++) {
+    usedDisplay = usedDisplay + usedLetters[i].toUpperCase() + " ";
+  };
+  document.querySelector("#title").innerHTML = "<h1>" + title + "</h1>";
+  document.querySelector("#maskedWord").innerHTML = "<p>" +  maskedWord + "</p>";
+  document.querySelector("#usedDisplay").innerHTML = "<p>Used Letters: " +  usedDisplay + "</p>";
+  document.querySelector("#alertDisplay").innerHTML = "<p>" +  alertDisplay + "</p>";
+  document.querySelector("#strikes").innerHTML = "<p>Strikes: " + strikes + " / 7</p>" + "</p>";
+  document.querySelector("#endGameAlert").innerHTML = "";
+  document.querySelector("#gamesWon").innerHTML =  "<p>" + gamesWon + "</p>";
+  document.querySelector("#hardcoreGamesWon").innerHTML =  "<p>" + hardcoreGamesWon + "</p>";
+
+};
+
+
+
+
+"Press any key to start!"
+//  render display
+
 //  Start Game
-    select new word
-      function selectWord() {
-        for (i = 0 ; i < selectWord.length ; i++) {
-          var letter = selectWord.charAt(i).toUpperCase();
-          currentWord.push(letter);
-        };
-      }
+function runGame {
+  //  select new word
+  function selectWord() {
+    for (i = 0 ; i < selectWord.length ; i++) {
+      var letter = selectWord.charAt(i).toUpperCase();
+      currentWord.push(letter);
+    }
+  }
+}
 //  render display
 
 
